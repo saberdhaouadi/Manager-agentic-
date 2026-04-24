480967c6ca2121375dd748f54af59f641d1ec1ef
Saber Dhaouadi <dhaouadis@gmail.com>
2026-04-24 11:20:10 +0100

    Refactor Vite config to remove server settings
    Removed preview and server configuration settings.

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: true,
-    port: parseInt(process.env.PORT) || 4173,
-    allowedHosts: 'all',
-  },
-  server: {
-    host: true,
-    port: parseInt(process.env.PORT) || 5173,
-    allowedHosts: 'all',
+  build: {
+    outDir: 'dist',
   }
 })
