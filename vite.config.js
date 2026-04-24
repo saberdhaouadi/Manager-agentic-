337772eaff060462cffad9d97984db13c15e924b
Saber Dhaouadi <dhaouadis@gmail.com>
2026-04-24 01:39:13 +0100

    Update Vite config to use boolean host and parse port

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
+    host: true,
+    port: parseInt(process.env.PORT) || 4173,
     allowedHosts: 'all',
   },
   server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
+    host: true,
+    port: parseInt(process.env.PORT) || 5173,
     allowedHosts: 'all',
   }
 })
